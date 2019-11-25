b7a59ee3da02ed33fbaeb9789b416a46188f2ce2
nikolaevvladik <54880046+nikolaevvladik@users.noreply.github.com>
2019-11-25 17:12:00 +1030

    Update loop-03.js

--- loop-03.js
+++ loop-03.js
@@ -1,10 +1,24 @@
-
-var gcd = function(m, n) {
-    if ( ! n) {
-        return m;
-    }
-
-    return gcd(n, m % n);
-};
-
+function gcd(m,n) {
+	var res;
+	let a;
+	let b;
+	
+	do {
+	a=Math.abs(n);
+	b=Math.abs(m);
+		
+		if (a>b){
+			n=a-b
+		}
+		if (a<b){
+			m=b-a
+		}
+		if (a==b){
+		res=a;
+		return res;}
+}
+while ((b>a) || (b<a))
+}
+	
+	
 module.exports = gcd;
